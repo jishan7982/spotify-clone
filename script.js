5abf0f03bed8e8931ceafe6c39c7eab49461635f
jishan7982 <mjjishan71@gmail.com>
2025-07-01 17:14:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("here the first JS web program start");
+
+// async function allSong() { 
+//     let a = await fetch("")
+//     let text = await a.text()
+//     let div = document.createElement("div")
+//     div.innerHTML = text;
+//     let as = div.getElementsByTagName("a")
+//     let songs = []
+//     for (let index = 0; index < as.length; index++) {
+//         const element = as[index];
+//         if (element.href.endsWith(".mp3")){
+//             songs.push(element.herf)
+//         }
+//         return songs;
+//     }
+// }
+// async function main() {
+//     let songs = await allSong();
+//     console.log(songs);
+
+//     var audio = new Audio(songs[0])
+//     audio.play();
+// }
+// main()
